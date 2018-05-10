cc2be14f92ae7cf9ce5be5beb8decf1b532817b6
shandamckinney <mckinney.shanda@gmail.com>
2018-05-10 18:25:36 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,9 @@
 function forLoop(array){
-  for (let i = 0, l = testArray.length; i < l; i++){
-    i === 0 console.log('I am ${i} strange loops.')
-    i === 1 console.log('I am 1 strange loop.')
+  for (let i = 0, i < 25; i++){
+    if (i === 1){
+      array.push('I am 1 strange loop.')
+    } else {
+      array.push('I am ${i} strange loops.')
   }
+  return array
 }
