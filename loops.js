362643dabc635828d43eb469dfe8aa4e7f72502d
shandamckinney <mckinney.shanda@gmail.com>
2018-05-10 18:22:00 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,6 @@
+function forLoop(array){
+  for (let i = 0, l = testArray.length; i < l; i++){
+    i === 0 console.log('I am ${i} strange loops.')
+    i === 1 console.log('I am 1 strange loop.')
+  }
+}
